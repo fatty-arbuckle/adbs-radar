4db1348db1fcc5f1d2cc2e1c142bc1197d3b4202
Fatty <fatty.arbuckle.87@gmail.com>
2019-06-29 23:19:35 -0400

    Updates to readme for uart

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ MIX_TARGET=host iex -S mix scenic.run
 
 MIX_TARGET=adsb_nerves_rpi3 mix deps.get
 MIX_TARGET=adsb_nerves_rpi3 mix firmware.burn
+
+# Connecting over UART
+
+Connect RX and TC to pins 8, 10
+
+Run `screen /dev/ttyUSB0 115200`
+
+## Pin Diagram
+
+![UART picture](uart_hd.jpg)
--- config/config.adsb_nerves_rpi3.exs
+++ config/config.adsb_nerves_rpi3.exs
@@ -7,18 +7,19 @@ config :dump_1090_client,
 config :adsb_radar, :viewport, %{
   name: :main_viewport,
   default_scene: {AdsbRadar.Scene.Radar, nil},
-  size: {1920, 1080},
+  # size: {1920, 1080},
+  size: {1600, 900},
   opts: [scale: 1.0],
   drivers: [
     %{
       module: Scenic.Driver.Nerves.Rpi
     },
-    %{
-      module: Scenic.Driver.Nerves.Touch,
-      opts: [
-        device: "FT5406 memory based driver",
-        calibration: {{1, 0, 0}, {1, 0, 0}}
-      ]
-    }
+    # %{
+    #   module: Scenic.Driver.Nerves.Touch,
+    #   opts: [
+    #     device: "FT5406 memory based driver",
+    #     calibration: {{1, 0, 0}, {1, 0, 0}}
+    #   ]
+    # }
   ]
 }
