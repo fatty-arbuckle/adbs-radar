4fcfb929abd3d9fb5f5b1cd45677c1633ee17b68
Fatty <fatty.arbuckle.87@gmail.com>
2019-07-07 00:06:02 -0400

    Refactors code and cleans up display

--- lib/scenes/radar.ex
+++ lib/scenes/radar.ex
@@ -2,14 +2,27 @@ defmodule AdsbRadar.Scene.Radar do
   use Scenic.Scene
   alias Scenic.Graph
   alias Scenic.ViewPort
-  import Scenic.Primitives, only: [circle: 3, line: 3, rect: 3, sector: 3, text: 3]
+  import Scenic.Primitives, only: [circle: 3, line: 3, sector: 3, text: 3]
 
-  @target_color :slate_gray
-  @active :lime
-  @dead :dark_gray
+  @target_color :dim_grey
+  @active {0x7C, 0xFC, 0x00}
+  @death_scale [
+    {0xbb, 0xbb, 0xb9},
+    {0x9c, 0x9c, 0x9c},
+    {0x7b, 0x7b, 0x7a},
+    {0x64, 0x64, 0x63},
+    {0x5c, 0x5c, 0x5b},
+    {0x4c, 0x4c, 0x4b},
+    {0x3c, 0x3c, 0x3b},
+    {0x30, 0x30, 0x2f}
+  ]
 
+  # convert these to settings passed in
+  @sweeper false
 
-  @frame_ms 26
+  # value for sweeper being on
+  # @frame_ms 26
+  @frame_ms 500
 
   # Constants
   @graph Graph.build(font: :roboto, font_size: 36)
@@ -17,6 +30,7 @@ defmodule AdsbRadar.Scene.Radar do
   # Initialize the game scene
   def init(_arg, opts) do
     viewport = opts[:viewport]
+
 
     {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(viewport)
 
@@ -31,6 +45,8 @@ defmodule AdsbRadar.Scene.Radar do
     # start a very simple animation timer
     {:ok, timer} = :timer.send_interval(@frame_ms, :frame)
 
+    padding = 10
+
     # The entire game state will be held here
     state = %{
       viewport: viewport,
@@ -44,22 +60,23 @@ defmodule AdsbRadar.Scene.Radar do
       objects: %{
         sweep: %{
           center: radar_center,
-          size: size,
+          size: size - padding,
           arc_location: 0,
           arc_length: 45.0,
-          speed: 1
+          speed: 1,
+          enabled: @sweeper,
         },
         target: %{
           center: radar_center,
-          size: size
+          size: size - padding,
         },
         indicator: %{
           center: radar_center,
-          size: size
+          size: size - padding,
         },
         aircraft: %{
           center: radar_center,
-          size: size,
+          size: size - padding,
           info_box: info_box
         }
       }
@@ -82,9 +99,9 @@ defmodule AdsbRadar.Scene.Radar do
   end
 
   defp draw_object(graph, :target, _frame, %{center: {cx, cy}, size: size}) do
-    stroke_width = 2
-    tick_length = 8
-    length = size - stroke_width
+    stroke_width = 1
+    tick_length = 5
+    length = (size - stroke_width)
 
     graph
     |> circle(length, stroke: {stroke_width, @target_color}, translate: {cx, cy})
@@ -95,7 +112,8 @@ defmodule AdsbRadar.Scene.Radar do
 
   end
 
-  defp draw_object(graph, :sweep, _frame, %{center: center, size: size, arc_location: arc_location, arc_length: arc_length}) do
+  defp draw_object(graph, :sweep, _frame, %{enabled: false}), do: graph
+  defp draw_object(graph, :sweep, _frame, %{center: center, size: size, arc_location: arc_location, arc_length: arc_length, enabled: true}) do
     parts = 12
     Enum.reduce(
       :lists.seq(0, parts - 1),
@@ -130,59 +148,86 @@ defmodule AdsbRadar.Scene.Radar do
   defp draw_object(graph, :aircraft, _frame, %{center: {cx, cy}, size: size, info_box: info_box}) do
 
     hangerInfo = Aircraft.Hanger.info()
-
-    idle_threshold = DateTime.to_unix(DateTime.utc_now) - 15
     hanger_data =
       Map.values(hangerInfo.aircraft)
       |> Enum.filter(fn bird ->
         bird.latitude != nil and bird.longitude != nil
       end)
-    if Enum.count(hanger_data) > 0 do
-      furtherest_bird = Enum.max_by(hanger_data, fn bird -> bird.distance end)
-      maxRange = fit_distance(furtherest_bird.distance)
+    graph |> draw_aircraft(hanger_data, hangerInfo.center, {cx, cy}, size, info_box)
+  end
 
-      # TODO pass in size to get this number
-      scale = maxRange / 390.0
+  defp draw_aircraft(graph, [], _, _, _, _), do: graph
+  defp draw_aircraft(graph, hanger_data, center, {cx, cy}, size, info_box) do
+    furtherest_bird = Enum.max_by(hanger_data, fn bird -> bird.distance end)
+    maxRange = fit_distance(furtherest_bird.distance)
 
-      graph = graph
-      |> text(Float.to_string(maxRange/1_000) <> " km", font: :roboto, font_size: 12, translate: {cx + 8 + size, cy} )
+    # TODO pass in size to get this number
+    scale = maxRange / 390.0
 
-      Enum.reduce(hanger_data, graph, fn bird, graph ->
-        stroke = if(bird.last_seen < idle_threshold, do: {1, @dead}, else: {3, @active})
-        x = (bird.distance/scale) * :math.cos(bird.bearing) + cx
-        y = (bird.distance/scale) * :math.sin(bird.bearing) + cy
+    graph = graph
+    |> text(Float.to_string(maxRange/1_000) <> " km", font: :roboto, font_size: 12, translate: {cx + 8 - size, cy - 8} )
 
-        label = if(bird.callsign != nil, do: bird.callsign, else: bird.icoa)
-        graph
-        |> circle( 5, stroke: stroke, translate: {x, y} )
-        |> text( label, font: :roboto, font_size: 16, translate: {x + 8, y - 3} )
-        |> draw_heading(bird.heading, bird.speed, x, y, stroke)
-        |> draw_path(hangerInfo.center, scale, [cx, cy], [x,y], bird.path)
-      end)
-      |> draw_info_box(info_box, hanger_data)
+    Enum.reduce(hanger_data, graph, fn bird, graph ->
+      x = (bird.distance/scale) * :math.cos(bird.bearing) + cx
+      y = (bird.distance/scale) * :math.sin(bird.bearing) + cy
 
-    else
-      graph
-    end
+      case get_color_by_age(bird.last_seen) do
+        nil ->
+          graph
+        fill_color ->
+          graph |> draw_bird(bird, center, scale, {cx, cy}, {x, y}, fill_color)
+      end
+    end)
+    |> draw_info_box(info_box, hanger_data)
+  end
+
+  defp draw_bird(graph, bird, center, scale, {cx, cy}, {x, y}, fill_color) do
+    graph
+    |> circle( 5, stroke: {1, fill_color}, translate: {x, y} )
+    |> text(label_string(bird.callsign, bird.icoa), font: :roboto, font_size: 16, fill: fill_color, translate: {x + 8, y - 3} )
+    |> draw_heading(bird.heading, bird.speed, x, y, {1, fill_color})
+    |> draw_path(center, scale, [cx, cy], [x,y], bird.path, {1, Tuple.append(fill_color, 0x50)})
   end
 
   defp draw_info_box(graph, {_origin, _size}, hanger_data) do
-    # stroke = {10, @active}
     hanger_data
     |> Enum.sort(fn a, b -> a.last_seen > b.last_seen end)
     |> Enum.take(25)
     |> Enum.with_index
     |> Enum.reduce(graph, fn {bird, i}, graph ->
-      label = if(bird.callsign != nil, do: bird.callsign, else: bird.icoa)
-      heading = "heading: #{bird.heading}"
-      speed = "speed: #{bird.speed}"
-      # bird_info = "#{label} lat: #{bird.latitude} long: #{bird.longitude} heading: #{bird.heading} speed: #{bird.speed}"
-      graph
-      # |> rect(size, stroke: stroke, translate: origin)
-      |> text( label, font: :roboto, font_size: 16, translate: {10, (20 * i) + 20} )
-      |> text( heading, font: :roboto, font_size: 16, translate: {80, (20 * i) + 20} )
-      |> text( speed, font: :roboto, font_size: 16, translate: {180, (20 * i) + 20} )
+      case get_color_by_age(bird.last_seen) do
+        nil ->
+          graph
+        fill_color ->
+          graph |> draw_info_row(i,
+            label_string(bird.callsign, bird.icoa),
+            heading_string(bird.heading),
+            speed_string(bird.speed),
+            altitude_string(bird.altitude),
+            fill_color
+          )
+      end
     end)
+  end
+
+  defp label_string(nil, icoa), do: icoa
+  defp label_string(callsign, _), do: callsign
+
+  defp heading_string(nil), do: "heading: ???°"
+  defp heading_string(heading), do: "heading: #{heading}°"
+
+  defp speed_string(nil), do: "speed: ???°"
+  defp speed_string(speed), do: "speed: #{speed} mph"
+
+  defp altitude_string(nil), do: "alt: ???°"
+  defp altitude_string(altitude), do: "alt: #{altitude} ft"
+
+  defp draw_info_row(graph, offset, label, heading, speed, altitude, fill_color) do
+    graph
+    |> text( label, font: :roboto, fill: fill_color, font_size: 16, translate: {10, (20 * offset) + 20} )
+    |> text( heading, font: :roboto, fill: fill_color, font_size: 16, translate: {80, (20 * offset) + 20} )
+    |> text( speed, font: :roboto, fill: fill_color, font_size: 16, translate: {170, (20 * offset) + 20} )
+    |> text( altitude, font: :roboto, fill: fill_color, font_size: 16, translate: {280, (20 * offset) + 20} )
   end
 
 
@@ -225,17 +270,17 @@ defmodule AdsbRadar.Scene.Radar do
     |> line({{heading_xs, heading_ys}, {heading_xe, heading_ye}}, stroke: stroke )
   end
 
-  defp draw_path(graph, center, scale, [cx, cy], [x, y], [ location | remaining]) do
+  defp draw_path(graph, center, scale, [cx, cy], [x, y], [ location | remaining], path_stroke) do
 
     bearing = Geocalc.bearing(center, location)
     distance = Geocalc.distance_between(center, location)
     nx = (distance/scale) * :math.cos(bearing) + cx
     ny = (distance/scale) * :math.sin(bearing) + cy
     graph
-    |> line({{x, y}, {nx, ny}}, stroke: {2, :dim_gray})
-    |> draw_path(center, scale, [cx, cy], [nx, ny], remaining)
+    |> line({{x, y}, {nx, ny}}, stroke: path_stroke)
+    |> draw_path(center, scale, [cx, cy], [nx, ny], remaining, path_stroke)
   end
-  defp draw_path(graph, _, _, _, _, []) do
+  defp draw_path(graph, _, _, _, _, [], _) do
     graph
   end
 
@@ -271,6 +316,24 @@ defmodule AdsbRadar.Scene.Radar do
 
   defp move_sweeper(%{objects: %{sweep: sweep}} = state) do
     put_in(state, [:objects, :sweep, :arc_location], rem((sweep.arc_location + sweep.speed), 360))
+  end
+
+  # Returns the color an aircraft should be rendered based on the last time
+  # it was seen.
+  # A nil value is returned if the aircraft is too old to render
+  defp get_color_by_age(last_seen) do
+    now = DateTime.to_unix(DateTime.utc_now)
+    case now - last_seen do
+      x when x > 300 -> Enum.at(@death_scale, 7)
+      x when x > 120 -> Enum.at(@death_scale, 6)
+      x when x >  60 -> Enum.at(@death_scale, 5)
+      x when x >  50 -> Enum.at(@death_scale, 4)
+      x when x >  40 -> Enum.at(@death_scale, 3)
+      x when x >  30 -> Enum.at(@death_scale, 2)
+      x when x >  20 -> Enum.at(@death_scale, 1)
+      x when x >  10  -> Enum.at(@death_scale, 0)
+      _             -> @active
+    end
   end
 
   defp fit_distance(f) when f <  25_000.0, do:  25_000.0
